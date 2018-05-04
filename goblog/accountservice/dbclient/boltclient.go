59aeb3acdd0f782407e62bba83fd745da2b740b3
Daniel Santos <drsantos20@gmail.com>
2018-05-04 15:43:27 -0300

    golang microservices api boilerplate

--- goblog/accountservice/dbclient/boltclient.go
+++ goblog/accountservice/dbclient/boltclient.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/boltdb/bolt"
 	"github.com/go-microservices/goblog/accountservice/model"
@@ -55,4 +56,49 @@ func (bc *BoltClient) OpenBoltDb() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Start seeding accounts
+func (bc *BoltClient) Seed() {
+	bc.initializeBucket()
+	bc.seedAccounts()
+}
+
+// Creates an "AccountBucket" in our BoltDB. It will overwrite any existing bucket of the same name.
+func (bc *BoltClient) initializeBucket() {
+	bc.boltDB.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte("AccountBucket"))
+		if err != nil {
+			return fmt.Errorf("create bucket failed: %s", err)
+		}
+		return nil
+	})
+}
+
+// Seed (n) make-believe account objects into the AcountBucket bucket.
+func (bc *BoltClient) seedAccounts() {
+
+	total := 100
+	for i := 0; i < total; i++ {
+
+		// Generate a key 10000 or larger
+		key := strconv.Itoa(10000 + i)
+
+		// Create an instance of our Account struct
+		acc := model.Account{
+			Id:   key,
+			Name: "Person_" + strconv.Itoa(i),
+		}
+
+		// Serialize the struct to JSON
+		jsonBytes, _ := json.Marshal(acc)
+
+		// Write the data to the AccountBucket
+		bc.boltDB.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("AccountBucket"))
+			err := b.Put([]byte(key), jsonBytes)
+			return err
+		})
+	}
+	fmt.Printf("Seeded %v fake accounts...\n", total)
 }
