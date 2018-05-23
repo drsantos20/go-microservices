82b8fe55533bca046d47d6421c5587a93bfacf29
Daniel Santos <drsantos20@gmail.com>
2018-05-23 11:38:54 -0300

    router and front react has been modified

--- front_app/src/components/Schedule.js
+++ front_app/src/components/Schedule.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Schedule = () => (
-  <div>
-    <ul>
-      <li>6/5 @ Evergreens</li>
-      <li>6/8 vs Kickers</li>
-      <li>6/14 @ United</li>
-    </ul>
-  </div>
-)
+class Schedule extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 0,
+      name: ""
+    };
+  }
 
+
+  componentDidMount() {
+
+    fetch('http://localhost:6767/accounts/10000')
+    .then(results => {
+      return results.json();
+    }).then(data => { 
+      console.log(data.id)
+      this.setState({id: data.id})
+      this.setState({name: data.name})
+
+    }).catch(err => {
+
+    });
+  }
+
+  render() {
+    return (
+      <div className="container2">
+        <div className="container1">
+          {this.state.id}
+          {this.state.name}
+        </div>
+      </div>
+    )
+  }
+}
 export default Schedule
